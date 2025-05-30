b37d8ca413a1c46fed6724b0a4c8aa01559c19e7
axellerE <85379828+axellerE@users.noreply.github.com>
2025-05-31 01:00:27 +0530

    Update route.ts

--- app/api/send-response/route.ts
+++ app/api/send-response/route.ts
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,  // Use SSL
   auth: {
-    user: process.env.EMAIL_USER=arindamroy2601@gmail.com
+    user: process.env.EMAIL_USER=arindamroy2601@gmail.com,
     pass: process.env.EMAIL_APP_PASSWORD=gjsf dsrl snyp dit
   }
 })
